7bcffca1842e6a206001c626a4c8f1be045da215
Raj Kumar Panda <rajkumar@stepsecurity.io>
2024-10-14 17:35:27 +0530

    build going through

--- esbuild.cjs
+++ esbuild.cjs
@@ -7,7 +7,7 @@ build({
     platform: 'node',
     entryPoints: ['src/index.ts'],
     outfile: 'dist/index.js',
-    target: 'node10',
+    target: 'node20', 
     loader: {
         '.md': 'text',
     },
